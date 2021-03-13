4cfa8f5d85cfd09b5018005bcc07ad0de2661d62
Alena Gritsiuk <alena22292@mail.ru>
2021-03-13 20:55:05 +0100

    There was generated two components: todo and todo-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'angular-todo-list';
-  name:string = 'Alena';
-
-  constructor(){
-    this.changeName('Alan');
-  }
-
-  changeName(name:string):void{
-    this.name = name;
-  }
+  name:string = 'Alan';
 }
