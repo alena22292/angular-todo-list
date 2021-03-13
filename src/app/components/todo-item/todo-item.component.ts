e67d7b450b98025ffcdf37f56fc07a9576636bcf
Alena Gritsiuk <alena22292@mail.ru>
2021-03-13 22:43:20 +0100

    Add functiion of changing boolean statement of the data from API fetch

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,3 +1,4 @@
+import { TodoService } from './../../services/todo.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from '../../models/Todo';
 
@@ -9,9 +10,35 @@ import { Todo } from '../../models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo!: Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed,
+    }
+
+    return classes;
+  }
+
+  // Change state of the todo.completed != todo.completed
+  onToggle(todo:any) {
+    // toggle in UI
+    todo.completed = !todo.completed;
+    console.log(todo.completed);
+    // toggle in server
+    this.todoService.toggleCompleted(todo).subscribe(data => {
+      console.log(data);
+    })
+
+  }
+
+  onDelete(todo:any) {
+    console.log('Delete', todo);
   }
 
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -9,26 +10,12 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
   todos!:Todo[];
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
-  ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: "Todo one",
-        completed: false,
-      },
-      {
-        id: 2,
-        title: "Todo two",
-        completed: false,
-      },
-      {
-        id: 3,
-        title: "Todo three",
-        completed: false,
-      }
-    ]
-  }
-
+  ngOnInit(){
+    this.todoService.getTodos().subscribe(data => {
+      this.todos = data;
+    });
+  } 
+  
 }
